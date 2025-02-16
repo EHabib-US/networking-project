a6288d0a46a0451da5b8429a7cc15ace901778d8
EHabib-US <50156314+EHabib-US@users.noreply.github.com>
2025-02-16 00:22:24 -0500

    fixed newline

--- process_form.php
+++ process_form.php
@@ -9,14 +9,14 @@ header("Refresh: 5; url=index.html");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["choice"])) {
         $choice = $_POST["choice"];
-        echo "You selected: " . htmlspecialchars($choice) . "<br>";
-        echo "Redirecting back to main site in 5 seconds.";
+        echo "You selected: " . htmlspecialchars($choice);
+        echo "\nRedirecting back to main site in 5 seconds.";
     } else {
         echo "No choice selected.";
-        echo "Redirecting back to main site in 5 seconds.<br>";
+        echo "\nRedirecting back to main site in 5 seconds.<br>";
     }
 } else {
     echo "Invalid request.";
-    echo "Redirecting back to main site in 5 seconds.<br>";
+    echo "\nRedirecting back to main site in 5 seconds.<br>";
 }
 ?>
