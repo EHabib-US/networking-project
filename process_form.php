3117cd67aa337a115acfe26e0360e282fa89add6
EHabib-US <50156314+EHabib-US@users.noreply.github.com>
2025-02-27 16:07:34 -0500

    fixed some text

--- process_form.php
+++ process_form.php
@@ -1,18 +1,20 @@
 <?php
+//Error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 //redirect after 5 seconds
-header("Refresh: 8; url=index.html");
+header("Refresh: 5; url=index.html");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 if (isset($_POST["choice"])) {
 $choice = $_POST["choice"];
-        echo "You selected: " . htmlspecialchars($choice) . " Redirecting back to the main page in 8 seconds...";
-        echo "You selected the color: " . htmlspecialchars($choice) . ". Redirecting back to the main page in 8 seconds...";
+        echo "You selected the color: " . htmlspecialchars($choice) . ". Redirecting back to the main page in 5 seconds...";
 } else {
-echo "No choice selected. Redirecting back to the main page in 8 seconds...";
+echo "No choice selected. Redirecting back to the main page in 5 seconds...";
 }
 } else {
-echo "Invalid request. Redirecting back to main site in 8 seconds...";
+echo "Invalid request. Redirecting back to main site in 5 seconds...";
 }
 
 //stops script execution after setting the header to prevent errors
